92d74cf8172cf3b271cdb5a6c78f6769e72667d5
AyanChoudhary <darkrider251099@gmail.com>
2019-07-25 20:15:43 +0530

    added candidate struct and candidae related functions

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,12 +16,17 @@ contract Election{
     bool _hasVoted;
   }
 
+  struct Candidate {
+    uint256 _id;
+  }
+
   mapping(uint256 => uint256) private idToVoter;
   mapping(bytes32 => uint256) private voteToCandidate;
   mapping(uint256 => uint256) private candidateToConstituency;
-  // mapping(uint256 => uint256) private constituencyCandidateCount;
+  mapping(uint256 => uint256) private constituencyCandidateCount;
 
   Voter[] public voters;
+  Candidate[] public candidates;
 
   function createVoter(uint256 _id, uint256 _voterId) private {
     uint id = voters.push(Voter(_id, _voterId)) - 1;
@@ -34,13 +39,18 @@ contract Election{
     constituencyCandidateCount[_constituencyId]++;
   }
 
-  // function getCandidatesOfConstituency(uint256 _constituencyId) external view returns(uint256[]) {
-  //   count = constituencyCandidateCount[_constituencyId];
-  //   uint256[] memory candidates = new uint256(count);
-  //   for (uint i = 0; i < voters.length; i++) {
+  function getCandidatesByConstituency(uint256 _constituencyId) external view returns(Candidate) {
+    Candidate[] memory _candidates = new Candidate(constituencyCandidateCount[_constituencyId]);
+    uint counter = 0;
+    for(uint i = 0; i < candidates.length; i++) {
+      if(candidateToConstituency[candidates[i]._id == _constituencyId]) {
+        _candidates[counter] = candidates[i];
+        counter++;
+      }
+    }
 
-  //   }
-  // }
+    return _candidates;
+  }
 
   address[] public deployedBallots;
   // constructor (bytes32[] memory candidates, bytes32[] memory district, uint hour) public {
