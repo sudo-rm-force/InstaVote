cec329ae28fbad5b303019fd898661d2bfa39f92
AyanChoudhary <darkrider251099@gmail.com>
2019-08-11 01:38:21 +0530

    made login component

--- client/src/components/landing/headerlanding.js
+++ client/src/components/landing/headerlanding.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import '../../styles/main.scss'
 
 class HeaderLanding extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        window.location = '/register'
+    }
+
     render() {
         return(
             <div className='headerlanding'>
@@ -11,7 +21,7 @@ class HeaderLanding extends Component {
                 <div className='headerlanding--heading'>
                     InstaVote
                 </div>
-                <button className='headerlanding--register'>Register</button>
+                <button className='headerlanding--register' onClick={this.handleClick}>Register</button>
             </div>
         )
     }
