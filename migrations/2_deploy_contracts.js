937718ffbf3de4ccc28fdacefcb3a52e77acaf65
AyanChoudhary <darkrider251099@gmail.com>
2019-08-12 02:41:00 +0530

    tried truffle migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,3 @@ var Election = artifacts.require("./Election.sol");
 module.exports = function(deployer) {
   deployer.deploy(Election);
 };
-
-// module.exports = (deployer, network, accounts) => {
-//   const userAddress = accounts[3];
-//   deployer.deploy(Election, [bytes32("subh"), bytes32("ammm")], [bytes32("amithi"), bytes32("raibareli")], uint(1))
-// }
