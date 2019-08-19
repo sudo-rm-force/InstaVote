290bf52a35885e576a39543887beffe817b9af3b
AyanChoudhary <darkrider251099@gmail.com>
2019-08-19 18:48:00 +0530

    fixed web3 config

--- admin/src/web3/web3-config.js
+++ admin/src/web3/web3-config.js
@@ -4,6 +4,6 @@ const web3 = new Web3(rpcURL)
 const contract = require('truffle-contract')
 const election_artifact = require('../contracts/Authorizer.json')
 const Election = contract(election_artifact)
-const election = web3.eth.Contract(Election.abi, '<address>')
+const election = new web3.eth.Contract(Election.abi, Election.networks.address)
 
 export default election
