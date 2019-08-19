b31333ccbdc85af25aea76052b692d9c628c813b
Subham103 <subhamsahoo4444@gmail.com>
2019-08-19 22:41:24 +0530

    some changes

--- client/src/components/registration/registrationform.js
+++ client/src/components/registration/registrationform.js
@@ -58,7 +58,7 @@ class RegistrationForm extends Component {
         //         })
         //     })
         // }) 
-        registerApi(this.state.VoterID, this.state.Name, this.state.Gender, this.state.MobileNo).then((res) => {
+        registerApi(this.state.VoterID, this.state.Name, this.state.Gender, this.state.MobileNo, this.state.FaceID).then((res) => {
             console.log(res)
         })
     }
